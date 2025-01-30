34cdaf6d322a4f9ab275a0e4a0deac90eae5ea9c
codesprut <alex-p.s@yandex.ru>
2025-01-31 01:17:02 +0500

    chore: eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-
-import { includeIgnoreFile } from "@eslint/compat";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import ts from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,10 +16,19 @@ const gitignorePath = path.resolve(__dirname, ".gitignore");
 export default [
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ts.configs.recommended,
+  ...ts.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   eslintPluginPrettierRecommended,
   {
-    files: ["**/*.ts", "**/*.js"],
+    files: ["*.ts", "*.js"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -28,6 +39,12 @@ export default [
       },
     },
     rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "import/no-nodejs-modules": "off",
       "prettier/prettier": "error",
       curly: "error",
     },
